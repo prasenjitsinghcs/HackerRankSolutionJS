ca433e76bcd2e11b2249267371409ea776098e53
Prasenjit Singh <prasenjit.singh@kore.com>
2024-05-14 17:03:17 +0530

    Brute Force: longest substring with k unique characters

--- sliding_window/longest_substring_with_k_unique_characters.js
+++ sliding_window/longest_substring_with_k_unique_characters.js
@@ -12,6 +12,46 @@
 
 var str = "aabacbebebe", k = 3;
 
+/**
+ * Approach 1: Brute Force Approach
+ */
+
+function getLongetSubStr(str, k) {
+    let low = 0, high = 0;
+    let max = Number.MIN_VALUE;
+    for(let i = 0; i < str.length; i++) {
+        for (let j = i; j < str.length; j++) {
+            if (j - i + 1 >= k) {
+                let index = i;
+                const distinct = new Set();
+                while(index <= j) {
+                    distinct.add(str[index]);
+                    index++;
+                }
+
+                if (distinct.size === k) {
+                    max = Math.max(max, j - i + 1);
+                    low = i;
+                    high = j;
+                }
+            }
+        }
+    }
+    console.log("Sub string = " + str.substr(low, high - low + 1));
+    return max;
+}
+
+console.log("Brute force Approach:");
+console.log(`Input: str = "${str}", k = ${k}`);
+var out = getLongetSubStr(str, k);
+console.log(`Output: ${out} (max length of the sub sting containing ${k} unique characters)`);
+
+// Time Complexity: O(n ^ 3), Since each character is processed by the left and right pointers exactly once.
+// Auxiliary Space: O(n)
+
+/**
+ * Approach 2: Sliding Window Approach
+ */
 function getLongetSubString(str, k) {
     let i = 0, j = 0, map = {}, max = Number.MIN_VALUE;
     let low = 0, high = 0;
@@ -53,7 +93,7 @@ function getLongetSubString(str, k) {
     return max;
 
 }
-
+console.log("\n\nSliding Window Approach:")
 console.log(`Input: str = "${str}", k = ${k}`);
 var out = getLongetSubString(str, k);
 console.log(`Output: ${out} (max length of the sub sting containing ${k} unique characters)`);
